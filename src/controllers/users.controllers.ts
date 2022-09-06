976f98167dbf33f0b6fa68b1569c01a233c1c09f
unknown <mateusbona28@gmail.com>
2022-09-06 14:45:50 -0300

    prod config

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 
 export const createUserController = async (req: Request, res: Response) => {
 
-    res.status(200).json("está funcionando 2!!!")
+    return res.status(200).json("está funcionando 2!!!")
 
 }
