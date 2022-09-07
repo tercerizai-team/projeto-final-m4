bd7702e48daf147f11969e87aae8486128df1d59
Tales <jobbtales@gmail.com>
2022-09-07 14:30:08 -0300

    refactoring

--- src/middlewares/authUser.middleware.ts
+++ src/middlewares/authUser.middleware.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import AppError from "../errors/AppError";
 
 export const authUserMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    try {
         const token = req.headers.authorization?.split(' ')[1]
+
+        if (!token) {
+            throw new AppError('Invalid token', 401)
+        }
+        
         jwt.verify(token as string, process.env.SECRET_KEY as string, (error: any, decoded: any) => {
             req.userEmail = decoded.email
             req.userId = decoded.userId
             req.userIsAdm = decoded.userIsAdm
             next()
         })
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Invalid token'
-        })
-    }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUserController } from "../controllers/users/createUser.controller";
 import { loginUserController } from "../controllers/users/loginUser.controller";
+import { authUserMiddleware } from "../middlewares/authUser.middleware";
 
 const usersRoutes = Router()
 
