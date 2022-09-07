e9d31bef115584eda4d3332e6ce56ac335cf479c
Tales <jobbtales@gmail.com>
2022-09-07 14:42:44 -0300

    feat: verify if is adm middleware

---
+++ src/middlewares/isAdm.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import AppError from "../errors/AppError";
+
+export const isAdmMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const userIsAdm = req.userIsAdm
+
+    if (userIsAdm === false) {
+        throw new AppError('Access denied', 403)
+    }
+
+    next()
+}
