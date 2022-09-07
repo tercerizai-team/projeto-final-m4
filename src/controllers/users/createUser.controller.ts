082d9efdae7fcd3b5f9f60313ca777cc43a02a94
Tales <jobbtales@gmail.com>
2022-09-07 12:54:08 -0300

    fix: renamed user var

--- src/controllers/users/createUser.controller.ts
+++ src/controllers/users/createUser.controller.ts
@@ -5,8 +5,8 @@ import { AppError, handleError } from "../../errors/AppError";
 export const createUserController = async (req: Request, res: Response) => {
     try {
         const { name, email, password, phone, isAdm } = req.body
-        const newUser = await createUserService({name, email, password, phone, isAdm})
-        return res.status(201).send(newUser)  
+        const user = await createUserService({name, email, password, phone, isAdm})
+        return res.status(201).send(user)  
     } catch (error) {
         if (error instanceof AppError) {
             handleError(error, res)
