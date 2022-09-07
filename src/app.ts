226d824a7088de0ea23a2837fca0314d7fa3e81f
Tales <jobbtales@gmail.com>
2022-09-07 14:07:32 -0300

    feat: added login route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "express-async-errors"
 import "reflect-metadata"
 import express from "express";
 import usersRoutes from "./routes/users.routes";
+import sessionRoutes from "./routes/session.routes"
 import { handleErrorMiddleware } from "./middlewares/errors.middleware";
 
 
@@ -9,6 +10,7 @@ const app = express()
 app.use(express.json())
 
 app.use("/users", usersRoutes)
+app.use("/login", sessionRoutes)
 
 app.use(handleErrorMiddleware)
 
