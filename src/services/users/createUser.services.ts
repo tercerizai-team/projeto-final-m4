834aaef8005f677b9c71fa9926da624af801b01c
unknown <mateusbona28@gmail.com>
2022-09-07 13:13:21 -0300

    fix: fix da rota que cria usuários, adicionado as chaves que estavam faltando para cadastrar no banco de dados

--- src/services/users/createUser.services.ts
+++ src/services/users/createUser.services.ts
@@ -13,10 +13,16 @@ export const createUserService = async ({name, email, password, phone, isAdm}: I
         throw new AppError(400, 'Email already being used')
     }
 
+    const createdAt = new Date()
+    const updatedAt = new Date()
+
     const user = new Users()
     user.name = name
     user.email = email
     user.password = await hash(password, 10)
+    user.createdAt = createdAt
+    user.updatedAt = updatedAt
+    user.isActive = true
     user.phone = phone
     user.isAdm = isAdm
 
