64a82e362fb76c39eafb5e5bd90ba0aca5c7d795
Willian <willianx64@gmail.com>
2022-09-08 11:08:29 -0300

    feat: rota de update

--- src/routes/provider.routes.ts
+++ src/routes/provider.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { listProvidersController } from "../controllers/providers/listProviders.controller";
+import { updateProviderController } from "../controllers/providers/updateProvider.controller";
 
 const providerRoutes = Router();
 
 providerRoutes.get('', listProvidersController)
+providerRoutes.patch('/:id', updateProviderController)
 
 export default providerRoutes;
