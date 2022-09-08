6edf659679a01c7d8e80e64bbbae4f5b17d8b6a0
Willian <willianx64@gmail.com>
2022-09-08 11:06:01 -0300

    fix: espera pela resposta do repositório

--- src/controllers/providers/listProviders.controller.ts
+++ src/controllers/providers/listProviders.controller.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 import { listProvidersService } from "../../services/providers/listProviders.service";
 
 export const listProvidersController = async (req: Request, res: Response) => {
-    const providers = listProvidersService()
+    const providers = await listProvidersService()
     return res.json(providers)
 }
--- src/services/providers/listProviders.service.ts
+++ src/services/providers/listProviders.service.ts
@@ -1,8 +1,8 @@
-import AppDataSource from "../../data-source"
-import { Providers } from "../../entities/providers.entity"
+import AppDataSource from "../../data-source";
+import { Providers } from "../../entities/providers.entity";
 
 export const listProvidersService = async () => {
-    const providerRepository = AppDataSource.getRepository(Providers)
-    const providers = providerRepository.find()
-    return providers
-}
+  const providerRepository = AppDataSource.getRepository(Providers);
+  const providers = await providerRepository.find();
+  return providers;
+};
