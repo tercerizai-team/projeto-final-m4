6a36a18c585a0029c39a4567b84327fc2df9e18a
Tales <jobbtales@gmail.com>
2022-09-08 19:22:37 -0300

    feat: edit categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createCategoryController } from "../controllers/categories/createCategory.controller";
 import { deleteCategoryController } from "../controllers/categories/deleteCategory.controller";
+import { editCategoryController } from "../controllers/categories/editCategory.controller";
 import { listCategoriesController } from "../controllers/categories/listCategories.controller";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 import { isAdmMiddleware } from "../middlewares/isAdm.middleware";
@@ -10,5 +11,6 @@ const categoriesRoutes = Router()
 categoriesRoutes.post("", authUserMiddleware, isAdmMiddleware, createCategoryController)
 categoriesRoutes.get("", listCategoriesController)
 categoriesRoutes.delete("/:id", authUserMiddleware, isAdmMiddleware, deleteCategoryController)
+categoriesRoutes.patch("/:id", editCategoryController)
 
 export default categoriesRoutes
