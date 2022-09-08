4f53750e744708349af73f8edb238ea963a958ce
Tales <jobbtales@gmail.com>
2022-09-08 18:08:48 -0300

    feat: list all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createCategoryController } from "../controllers/categories/createCategory.controller";
+import { listCategoriesController } from "../controllers/categories/listCategories.controller";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 import { isAdmMiddleware } from "../middlewares/isAdm.middleware";
 
 const categoriesRoutes = Router()
 
 categoriesRoutes.post("", authUserMiddleware, isAdmMiddleware, createCategoryController)
+categoriesRoutes.get("", listCategoriesController)
 
 export default categoriesRoutes
