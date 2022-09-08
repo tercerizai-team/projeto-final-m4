617d6a8a4d4404ba40fa5b992d981550266b3c6e
Tales <jobbtales@gmail.com>
2022-09-08 10:04:00 -0300

    feat: added providers routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import "reflect-metadata"
 import express from "express";
 import usersRoutes from "./routes/users.routes";
 import sessionRoutes from "./routes/session.routes"
+import providersRoutes from "./routes/providers.routes"
 import { handleErrorMiddleware } from "./middlewares/errors.middleware";
 
 
@@ -11,6 +12,7 @@ app.use(express.json())
 
 app.use("/users", usersRoutes)
 app.use("/login", sessionRoutes)
+app.use("/providers", providersRoutes)
 
 app.use(handleErrorMiddleware)
 
