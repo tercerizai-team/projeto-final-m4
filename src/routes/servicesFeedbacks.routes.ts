6ec44a81091038c389dc5d774ffe5121806d7f93
Willian <willianx64@gmail.com>
2022-09-09 11:05:37 -0300

    feat: listar feedbacks do provider

--- src/routes/servicesFeedbacks.routes.ts
+++ src/routes/servicesFeedbacks.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createServiceFeedbackController } from "../controllers/servicesFeedbacks/createServiceFeedback.controller";
+import { listProviderFeedbacksController } from "../controllers/servicesFeedbacks/listProviderFeedbacks.controller";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 
 const servicesFeedbacksRoutes = Router();
 
 servicesFeedbacksRoutes.post("", authUserMiddleware, createServiceFeedbackController)
+servicesFeedbacksRoutes.get("/:id", authUserMiddleware, listProviderFeedbacksController)
 
 export default servicesFeedbacksRoutes;
