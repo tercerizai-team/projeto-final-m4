53d47ffb687d86c7b466634fcc9595a43852c5ba
unknown <mateusbona28@gmail.com>
2022-09-09 11:25:23 -0300

    feat: testes de integração para usuários admin/não admin completos

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,1 +1,24 @@
 //aqui ficam os Mocks para testes
+
+
+//users
+
+export const mockedUserNotAdm = {
+    name: "Jorgin",
+    email: "jorgin@mail.com",
+    password: "1234",
+    phone: "4111111111",
+    isAdm: false
+}
+
+export const mockedUserAdm = {
+    name: "Jorge",
+    email: "jorge@mail.com",
+    password: "1234",
+    phone: "4111111111",
+    isAdm: true
+}
+
+export const mockedUserUpdate = {
+    name: "Mateus"
+}
