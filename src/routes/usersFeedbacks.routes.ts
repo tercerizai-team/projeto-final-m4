80585911401c4000ce99871da6c4d57d1417250c
Willian <willianx64@gmail.com>
2022-09-09 05:42:33 -0300

    feat: atualizar feedbacks de usuário

--- src/routes/usersFeedbacks.routes.ts
+++ src/routes/usersFeedbacks.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { createUserFeedbackController } from "../controllers/usersFeedbacks/createUserFeedback.controller";
 import { listUserFeedbacksController } from "../controllers/usersFeedbacks/listUserFeedbacks.controller";
+import { updateUserFeedbackController } from "../controllers/usersFeedbacks/updateUserFeedback.controller";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
+import { isAdmMiddleware } from "../middlewares/isAdm.middleware";
 
 const usersFeedbacksRoutes = Router();
 
 usersFeedbacksRoutes.post('', authUserMiddleware, createUserFeedbackController)
 usersFeedbacksRoutes.get('/:id', authUserMiddleware, listUserFeedbacksController)
+usersFeedbacksRoutes.patch('/:id', authUserMiddleware, isAdmMiddleware, updateUserFeedbackController)
 
 export default usersFeedbacksRoutes;
