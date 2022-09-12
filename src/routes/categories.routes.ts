174fae8731b3052cd842d5115a38c73ad959ae1e
Tales <jobbtales@gmail.com>
2022-09-12 15:23:52 -0300

    feat: get category by id

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createCategoryController } from "../controllers/categories/createCategory.controller";
 import { deleteCategoryController } from "../controllers/categories/deleteCategory.controller";
 import { editCategoryController } from "../controllers/categories/editCategory.controller";
+import { getCategoryController } from "../controllers/categories/getCategory.controller";
 import { listCategoriesController } from "../controllers/categories/listCategories.controller";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 import { isAdmMiddleware } from "../middlewares/isAdm.middleware";
@@ -10,6 +11,7 @@ const categoriesRoutes = Router()
 
 categoriesRoutes.post("", authUserMiddleware, isAdmMiddleware, createCategoryController)
 categoriesRoutes.get("", listCategoriesController)
+categoriesRoutes.get("/:id", getCategoryController)
 categoriesRoutes.delete("/:id", authUserMiddleware, isAdmMiddleware, deleteCategoryController)
 categoriesRoutes.patch("/:id", authUserMiddleware, isAdmMiddleware, editCategoryController)
 
