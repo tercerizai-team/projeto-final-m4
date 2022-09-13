8a5a1b7dbf726e4572af7266f75f9da951de72cc
Willian <willianx64@gmail.com>
2022-09-13 16:29:54 -0300

    fix: Mensagens específicas de erro na deleção

--- src/services/categories/deleteProviderCategory.services.ts
+++ src/services/categories/deleteProviderCategory.services.ts
@@ -1,20 +1,37 @@
 import AppDataSource from "../../data-source"
+import { Categories } from "../../entities/categories.entity";
 import { CategoryProvider } from "../../entities/category_provider.entity"
+import { Providers } from "../../entities/providers.entity";
 import AppError from "../../errors/AppError"
 
 
 export const deleteProviderCategoryService = async (providerId: string, id: string): Promise<void> => {
 
-    const providerCategoryRepository = AppDataSource.getRepository(CategoryProvider)
+    const providerCategoryRepository =
+    AppDataSource.getRepository(CategoryProvider);
+  const providersRepository = AppDataSource.getRepository(Providers);
+  const categoryRepository = AppDataSource.getRepository(Categories);
 
-    const allProviderCategories = await providerCategoryRepository.find({relations: {provider: true}})
+  const provider = await providersRepository.findOneBy({ id: providerId });
 
-    const categoryToDelete:any = allProviderCategories.find(provCategory => provCategory.provider.id === providerId && provCategory.category.id === id)
+  if (!provider) {
+    throw new AppError("User not provider");
+  }
 
-    if(!allProviderCategories){
-        throw new AppError("invalid provider id or category id", 404);
-    }
+  const category = await categoryRepository.findOneBy({ id });
 
-    await providerCategoryRepository.delete(categoryToDelete?.id)
+  if (!category) {
+    throw new AppError("Invalid category", 404);
+  }
+
+  const categoryProvider = await providerCategoryRepository.findOne({
+    where: {category: {id: category.id}, provider: {id: provider.id}},
+  });
+
+  if(!categoryProvider) {
+    throw new AppError("Category does not belong to the user")
+  }
+
+   await providerCategoryRepository.delete(categoryProvider.id)
 
 }
