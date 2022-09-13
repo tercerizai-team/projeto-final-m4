129fede12cdab782f12015b68c3ecbda2a770802
matheuswillcox <matheuswillcox@gmail.com>
2022-09-13 19:05:23 -0300

    feat/getServices

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createServiceController } from "../controllers/service/createService.controller";
+import getServicesController from "../controllers/service/getServices.controller";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 
 const servicesRoutes = Router();
 
 servicesRoutes.post("", authUserMiddleware, createServiceController);
+servicesRoutes.get("", authUserMiddleware,getServicesController);
 
 export default servicesRoutes;
---
+++ src/services/service/getServicesService.ts
@@ -0,0 +1,42 @@
+import AppDataSource from "../../data-source";
+import AppError from "../../errors/AppError";
+import { Schedules } from "../../entities/schedules.entity";
+import { Services } from "../../entities/services.entity";
+
+const getServicesService = async (userId: string, userIsAdm: boolean) => {
+  const schedulesRepository = AppDataSource.getRepository(Schedules);
+  const servicesRepository = AppDataSource.getRepository(Services);
+
+  const schedules = await schedulesRepository.find({
+    relations: { provider: true, user: true },
+  });
+  const services = await servicesRepository.find({
+    relations: { schedule: { user: true, provider: true } },
+  });
+
+  if (userIsAdm === true) {
+    return services;
+  }
+
+  const userScheduleToGet = services.find((elem) => elem.schedule.user.id === userId);
+  const providerScheduleToGet = services.find(
+    (elem) =>  elem.schedule.provider.id === userId
+  );
+
+  if (userScheduleToGet) {
+    return userScheduleToGet.schedule.user;
+  }
+  if (providerScheduleToGet) {
+    return providerScheduleToGet.schedule.provider;
+  }
+
+  if (!userScheduleToGet) {
+    throw new AppError("User not found", 404);
+  }
+
+  if (!providerScheduleToGet) {
+    throw new AppError("Provider not found", 404);
+  }
+};
+
+export default getServicesService;
