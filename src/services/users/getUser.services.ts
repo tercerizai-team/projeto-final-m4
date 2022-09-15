2bfddba30ba66cff6caf39d3f9e0b4e725ec42ae
unknown <mateusbona28@gmail.com>
2022-09-15 09:25:22 -0300

    fix: users para main

--- src/services/users/getUser.services.ts
+++ src/services/users/getUser.services.ts
@@ -5,7 +5,7 @@ import AppError from "../../errors/AppError";
 export const getUserService = async (id: string) => {
     const userRepository = AppDataSource.getRepository(Users)
     const usersAddresses = await userRepository.find({relations: {addresses: true}})
-    const userAddresses = await usersAddresses.filter(userAddress => userAddress.addresses.filter(address => address.id === id))
+    const userAddresses = usersAddresses.filter(userAddress => userAddress.addresses.filter(address => address.id === id))
 
     if (!usersAddresses) {
         throw new AppError('User not found', 404)
